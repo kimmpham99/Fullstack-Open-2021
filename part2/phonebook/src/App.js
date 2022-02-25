830e5442cc110cef8916c2dbd8116463332f5335
kimpham315 <85162373+kimpham315@users.noreply.github.com>
2022-02-25 10:16:22 +0700

    using axios + useEffect to fetch data from server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Display from './components/Display'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filterName, setFilterName] = useState('')
+
+    useEffect(() => {
+        console.log('trigger function in useEffect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(reponse => {
+                console.log('promise fullfilled')
+                setPersons(reponse.data)
+            })
+    }, [])
 
     //add new contact
     const addContact = (event) => {
@@ -57,7 +63,7 @@ const App = () => {
         <div>
             <h2>Phonebook</h2>
 
-            <Filter filterName={filterName} filterContactName={filterContactName}/>
+            <Filter filterName={filterName} filterContactName={filterContactName} />
 
             <h2>add a new</h2>
             <PersonForm
@@ -69,7 +75,7 @@ const App = () => {
             />
 
             <h2>Numbers</h2>
-            <Display contactToShow={contactToShow}/>
+            <Display contactToShow={contactToShow} />
         </div>
     )
 }
