ffe0af1384593e551e6d5a5633c7205691ce9ea9
kimpham315 <85162373+kimpham315@users.noreply.github.com>
2022-02-27 15:57:55 +0700

    finish 2.13 part 2

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -4,33 +4,40 @@ import Display from './components/Display'
 import Filter from './components/Filter'
 
 const App = () => {
-	const [countries, setCountries] = useState([])
-	const [filterName, setFilterName] = useState('Input country')
+    const [countries, setCountries] = useState([])
+    const [filterName, setFilterName] = useState('Input country')
+    const [showOnClick, setShowOnClick] = useState([])
 
-	useEffect(() => {
-        console.log('trigger function in useEffect')
+    useEffect(() => {
         axios
             .get('https://restcountries.com/v3.1/all')
             .then(reponse => {
-                console.log('promise fullfilled')
-				console.log(reponse.data)
                 setCountries(reponse.data)
             })
     }, [])
 
-	//filter
+    //filter search
     const filterCountryName = (event) => {
         setFilterName(event.target.value)
+        setShowOnClick([])
+    }
+
+    const showCountryInfoHandle = (country) => {
+        setShowOnClick(showOnClick.concat(country.name.common))
     }
 
     const countryToShow = countries.filter(country => (country.name.common.toLowerCase()).includes(filterName.toLowerCase()))
 
-	return (
-		<div>
+    return (
+        <div>
             <Filter filterName={filterName} filterCountryName={filterCountryName} />
-			<Display countryToShow={countryToShow} />
-		</div>
-	)
+            <Display
+                countryToShow={countryToShow}
+                showCountryInfoHandle={showCountryInfoHandle}
+                showOnClick={showOnClick}
+            />
+        </div>
+    )
 }
 
 export default App;
--- part2/countries/src/components/Display.js
+++ part2/countries/src/components/Display.js
@@ -1,40 +1,64 @@
-const Display = ({ countryToShow }) => {
+const Display = ({ countryToShow, showCountryInfoHandle, showOnClick }) => {
     if (countryToShow.length > 10) {
         return (
             <p>Too many matches, specify another filter</p>
         )
-    } 
-    
+    }
+
     else if (countryToShow.length > 1 && countryToShow.length <= 10) {
         return (
             <div>
                 {countryToShow.map(country =>
-                    <MatchCountries key={country.cca3} country={country} />
+                    <MatchCountries
+                        key={country.cca3}
+                        country={country}
+                        showCountryInfoHandle={showCountryInfoHandle}
+                        showOnClick={showOnClick}
+                    />
                 )}
             </div>
         )
     }
-    
-    else if (countryToShow.length === 1){
-        return(
+
+    else if (countryToShow.length === 1) {
+        return (
             <CountryInformation country={countryToShow[0]} />
         )
     }
-    
-    else{
+
+    else {
         return <p></p>
     }
 }
 
-const MatchCountries = ({ country }) => {
+const MatchCountries = ({ country, showCountryInfoHandle, showOnClick }) => {
+    if (showOnClick.includes(country.name.common)) {
+        return (
+            <div>
+                <p>
+                    {country.name.common}
+                    <button onClick={() => showCountryInfoHandle(country)}>
+                        show
+                    </button>
+                </p>
+                <CountryInformation country={country} />
+            </div>
+        )
+    }
+
     return (
-        <p>{country.name.common}</p>
+        <div>
+            <p>
+                {country.name.common}
+                <button onClick={() => showCountryInfoHandle(country)}>
+                    show
+                </button>
+            </p>
+        </div>
     )
 }
 
-const CountryInformation = ({country}) => {
-    console.log(country)
-    console.log(country.languages)
+const CountryInformation = ({ country }) => {
     return (
         <div>
             <h2>{country.name.common}</h2>
