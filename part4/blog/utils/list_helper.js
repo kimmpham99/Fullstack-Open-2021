bf254c638c7d6421716f6375b3e241caa8975480
kimpham315 <phamanhkim1999@gmail.com>
2022-03-09 12:07:27 +0700

    finish 4.5 part 4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,7 +6,25 @@ const totalLikes = (blogsList) => {
   return blogsList.reduce((previous, current) => previous + current.likes, 0)
 }
 
+const mostBlogs = (blogsList) => {
+
+  console.log(blogsList.reduce((previous, current) => {
+    if (previous.likes > current.likes) {
+      return previous
+    }
+    return current
+  }, { likes: 0 }))
+
+  return blogsList.reduce((previous, current) => {
+    if (previous.likes > current.likes) {
+      return previous
+    }
+    return current
+  }, { likes: 0 })
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  mostBlogs,
 }
