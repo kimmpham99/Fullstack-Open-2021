acbc345ec3d4e5c67e6a9028f18f118b5e7682f8
kimpham315 <phamanhkim1999@gmail.com>
2022-03-14 23:07:47 +0700

    finish 4.14 part 4b

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const { request, response } = require('../app')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
@@ -22,6 +23,29 @@ blogsRouter.post('/', async (request, response) => {
     const savedBlog = await blog.save()
     response.status(201).json(savedBlog)
   }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async(request, response) => {
+  const body = request.body
+
+  if (!body.title && !body.url) {
+    response.status(400).end()
+  } else {
+    const blog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes
+    }
+
+    const changedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+    response.json(changedBlog)
+  } 
 })
 
 module.exports = blogsRouter
--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -67,12 +67,12 @@ test('create a new blog post', async () => {
 
   const response = await api.get('/api/blogs')
 
-  const contents = response.body.map(r => r.title)
+  const titles = response.body.map(r => r.title)
 
   //total number check
   expect(response.body).toHaveLength(initialBlogs.length + 1)
   //new post check
-  expect(contents).toContain('C')
+  expect(titles).toContain('C')
 })
 
 //4.11 - blogs without likes -> like = 0
@@ -112,6 +112,60 @@ test('blog without title and url is not added', async () => {
   const response = await api.get('/api/blogs')
 
   expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+//4.13 - deleting a single blog post resource
+test('a blog can be deleted', async () => {
+  const blogsAtStart = await api.get('/api/blogs')
+
+  //delete first blog post
+  const blogToDelete = blogsAtStart.body[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await api.get('/api/blogs')
+
+  expect(blogsAtEnd.body).toHaveLength(initialBlogs.length - 1)
+
+  const titles = blogsAtEnd.body.map(r => r.title)
+
+  expect(titles).not.toContain(blogToDelete.title)
+})
+
+//4.14 - updating a single blog post
+test('updating a blog post', async () => {
+  const blogsAtStart = await api.get('/api/blogs')
+
+  //change first blog post
+  const blogToChange = blogsAtStart.body[0]
+
+  const changedBlog = {
+    title: 'A_changed',
+    author: 'Kim_changed',
+    url: 'web_A_changed',
+    likes: 40
+  }
+
+  await api
+    .put(`/api/blogs/${blogToChange.id}`)
+    .send(changedBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+  const authors = response.body.map(r => r.author)
+  const urls = response.body.map(r => r.url)
+  const likes = response.body.map(r => r.likes)
+
+  //changed post check
+  expect(titles).toContain('A_changed')
+  expect(authors).toContain('Kim_changed')
+  expect(urls).toContain('web_A_changed')
+  expect(likes).toContain(40)
 })
 
 afterAll(() => {
