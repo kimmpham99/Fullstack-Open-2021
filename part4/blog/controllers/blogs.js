17319ff18c9f279231db5a3e8f152204cd807777
kimpham315 <phamanhkim1999@gmail.com>
2022-03-22 22:50:33 +0700

    finish part 4 except 4.23

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,9 +1,5 @@
 const blogsRouter = require('express').Router()
-const { request, response } = require('../app')
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
-const { userExtractor } = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -16,19 +12,13 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  /*const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
-  const user = await User.findById(decodedToken.id)*/
-
   const user = request.user
   if (request.user === null) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
 
   if (!body.title && !body.url) {
-    response.status(400).end()
+    return response.status(400).end()
   } else {
     const blog = new Blog({
       title: body.title,
@@ -47,13 +37,6 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-
-  /*const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
-
-  const user = await User.findById(decodedToken.id)*/
 
   const user = request.user
   if (request.user === null) {
@@ -83,23 +66,20 @@ blogsRouter.put('/:id', async (request, response) => {
 
   const blog = await Blog.findById(request.params.id)
 
-  if (!body.title && !body.url) {
-    return response.status(400).end()
-  } else {
-    const blogToChange = {
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes
-    }
-
-    if (blog.user.toString() === user._id.toString()) {
-      const changedBlog = await Blog.findByIdAndUpdate(request.params.id, blogToChange, { new: true })
-      return response.json(changedBlog)
-    }
-
-    response.status(401).json({ error: 'permission denied' })
+  const blogToChange = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
   }
+
+  if (blog.user.toString() === user._id.toString()) {
+    const changedBlog = await Blog.findByIdAndUpdate(request.params.id, blogToChange, { new: true })
+    return response.json(changedBlog)
+  }
+
+  response.status(401).json({ error: 'permission denied' })
+
 })
 
 module.exports = blogsRouter
